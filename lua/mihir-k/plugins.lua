3da774cc85b34f68b38d0ade891eb28978a4e566
Mihir Karbelkar <karbelkar.mihir@gmail.com>
2023-12-06 22:53:45 +0530

    add format on save and format

--- lua/mihir-k/plugins.lua
+++ lua/mihir-k/plugins.lua
@@ -1,38 +1,39 @@
 return {
-  "folke/neodev.nvim",
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  'williamboman/mason-lspconfig.nvim',
-  {
-  'charludo/projectmgr.nvim',
-  lazy = false, -- important!
-},{ 'dasupradyumna/midnight.nvim', lazy = false, priority = 1000 },{
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.4',
-  dependencies = { 'nvim-lua/plenary.nvim' }
+	"folke/neodev.nvim",
+	"folke/which-key.nvim",
+	{ "folke/neoconf.nvim",          cmd = "Neoconf" },
+	'williamboman/mason-lspconfig.nvim',
+	{
+		'charludo/projectmgr.nvim',
+		lazy = false, -- important!
+	}, { 'dasupradyumna/midnight.nvim', lazy = false, priority = 1000 }, {
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.4',
+	dependencies = { 'nvim-lua/plenary.nvim' }
 },
-{'neovim/nvim-lspconfig'},
-{'hrsh7th/cmp-nvim-lsp'},
-{'hrsh7th/nvim-cmp'},
-{'L3MON4D3/LuaSnip'},
-{'williamboman/mason-lspconfig.nvim'}
-,{
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },{
-    "williamboman/mason.nvim"
-},{'ThePrimeagen/harpoon', branch = 'harpoon2', requires =  {"nvim-lua/plenary.nvim"} },{'christoomey/vim-tmux-navigator'},{
-           "kdheepak/lazygit.nvim",
-    requires = {
-        "nvim-telescope/telescope.nvim",
-        "nvim-lua/plenary.nvim",
-    },
-    config = function()
-	require('telescope').setup({ file_ignore_patterns = { "node%_modules/.*" } })
-        require("telescope").load_extension("lazygit")
-    end    }
+	{ 'neovim/nvim-lspconfig' },
+	{ 'hrsh7th/cmp-nvim-lsp' },
+	{ 'hrsh7th/nvim-cmp' },
+	{ 'L3MON4D3/LuaSnip' },
+	{ 'williamboman/mason-lspconfig.nvim' }
+	, {
+	-- Add indentation guides even on blank lines
+	'lukas-reineke/indent-blankline.nvim',
+	-- Enable `lukas-reineke/indent-blankline.nvim`
+	-- See `:help ibl`
+	main = 'ibl',
+	opts = {},
+}, {
+	"williamboman/mason.nvim"
+}, { 'ThePrimeagen/harpoon',    branch = 'harpoon2', requires = { "nvim-lua/plenary.nvim" } }, { 'christoomey/vim-tmux-navigator' }, {
+	"kdheepak/lazygit.nvim",
+	requires = {
+		"nvim-telescope/telescope.nvim",
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		require('telescope').setup({ file_ignore_patterns = { "node%_modules/.*" } })
+		require("telescope").load_extension("lazygit")
+	end
+}, { "elentok/format-on-save.nvim" }, { "rebelot/kanagawa.nvim" }
 }
--- lua/mihir-k/plugins_config/init.lua
+++ lua/mihir-k/plugins_config/init.lua
@@ -1,3 +1,4 @@
 require('mihir-k.plugins_config.mason')
 require('mihir-k.plugins_config.lsp')
 require('mihir-k.plugins_config.treesitter')
+require('mihir-k.plugins_config.format-on-save')
